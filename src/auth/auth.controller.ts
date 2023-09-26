4733bcf71e1d8f2f7ae2af7962d5cda08866f06c
Jefferson Ventura <jeffersonsilvamuz@gmail.com>
2023-09-26 00:51:35 -0300

    feat(auth): create login route in AuthController

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,4 +1,12 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Post, Body } from '@nestjs/common';
+import { AuthService } from './auth.service';
 
 @Controller('auth')
-export class AuthController {}
+export class AuthController {
+    constructor(private authService: AuthService) { }
+
+    @Post('/login')
+    async login(@Body() body) {
+        return this.authService.validarUsuario(body.username, body.password);
+    }
+}
