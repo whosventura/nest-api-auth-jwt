611dafd42e61019e41ac2f5f671236c161b92cdd
Jefferson Ventura <jeffersonsilvamuz@gmail.com>
2023-09-25 22:20:52 -0300

    feat(auth): created module, service, and controller for authentication

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,9 +4,10 @@ import { AppService } from './app.service';
 import { UsersModule } from './users/users.module';
 import typeOrmConfig from './typeorm.config';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import { AuthModule } from './auth/auth.module';
 
 @Module({
-  imports: [UsersModule, TypeOrmModule.forRoot(typeOrmConfig)],
+  imports: [UsersModule, TypeOrmModule.forRoot(typeOrmConfig), AuthModule],
   controllers: [AppController],
   providers: [AppService],
 })
