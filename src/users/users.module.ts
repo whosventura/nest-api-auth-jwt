2bef6764d6199b5df15ac90dfcfc00bd52021e3e
Jefferson Ventura <jeffersonsilvamuz@gmail.com>
2023-09-26 17:03:47 -0300

    feat: Add JWT module configuration and strategy to UserModule and AuthService

--- src/users/users.module.ts
+++ src/users/users.module.ts
@@ -3,10 +3,20 @@ import { UsersService } from './users.service';
 import { UsersController } from './users.controller';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { User } from './entities/user.entity';
+import { JwtModule } from '@nestjs/jwt';
+import { AuthService } from 'src/auth/auth.service';
+import { JwtStrategy } from 'src/auth/strategies/jwt-strategy';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([User])],
-  providers: [UsersService],
+  imports: [
+    TypeOrmModule.forFeature([User]),
+    JwtModule.register({})
+  ],
+  providers: [
+    UsersService,
+    AuthService,
+    JwtStrategy
+  ],
   controllers: [UsersController],
   exports: [UsersService],
 })
