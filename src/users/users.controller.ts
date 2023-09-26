6aed01fd86ce5de8a86290160e387185cf22d6b0
Jefferson Ventura <jeffersonsilvamuz@gmail.com>
2023-09-26 17:04:51 -0300

    feat: Add JWT authentication guard to listarUsuarios endpoint

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,7 +1,8 @@
-import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/common';
+import { Controller, Get, Post, Body, Patch, Param, Delete, UseGuards } from '@nestjs/common';
 import { UsersService } from './users.service';
 import { CreateUserDto } from './dto/create-user.dto';
 import { UpdateUserDto } from './dto/update-user.dto';
+import { AuthGuard } from '@nestjs/passport';
 
 @Controller('users')
 export class UsersController {
@@ -13,7 +14,8 @@ export class UsersController {
   }
 
   @Get()
-  findAll() {
+  @UseGuards(AuthGuard('jwt'))
+  listarUsuarios() {
     return this.usersService.findAll();
   }
 
