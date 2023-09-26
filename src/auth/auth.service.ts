0b18f60343962bf1db9c813b5cbda70fc30dd967
Jefferson Ventura <jeffersonsilvamuz@gmail.com>
2023-09-26 00:50:38 -0300

    feat(auth): add constructor, validarUsuario and gerarToken method to AuthService

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,4 +1,35 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, UnauthorizedException } from '@nestjs/common';
+import { JwtService } from '@nestjs/jwt';
+import { User } from 'src/users/entities/user.entity';
+import { UsersService } from 'src/users/users.service';
 
 @Injectable()
-export class AuthService {}
+export class AuthService {
+    constructor(
+        private readonly usersService: UsersService,
+        private jwtService: JwtService,
+    ) { }
+
+    async validarUsuario(username: string, password: string): Promise<any> {
+        const user = await this.usersService.findOneByEmail(username);
+        if (!user) {
+            throw new UnauthorizedException('Usuário ou Senha inválidos');
+        }
+        if (user.password === password) {
+            return await this.gerarToken(user);
+        }
+        throw new UnauthorizedException('Usuário ou Senha inválidos');
+    }
+
+    async gerarToken(payload: User) {
+        return {
+            access_token: this.jwtService.sign(
+                { email: payload.email },
+                {
+                    secret: 'topSecret512',
+                    expiresIn: '50s',
+                },
+            ),
+        };
+    }
+}
